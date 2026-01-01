e7d4e632e64c0fa764bceae04934a8ff125a9fb3
2025128281-Hakimi <2025128281@student.uitm.edu.my>
2026-01-01 21:03:25 +0800

    change

--- backend/routes/report.routes.js
+++ backend/routes/report.routes.js
@@ -2,13 +2,6 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/financialReport");
 
-<<<<<<< HEAD
 router.get("/financial-report", financialController.financialReport);
-=======
-/*
-  GET /report?month=2025-01
-*/
-router.get("/", controller.financialReport);
->>>>>>> 560af3b7698e643aeafd73fdf043426f80db06f5
 
 module.exports = router;
