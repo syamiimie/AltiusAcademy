e9efeccd94a86d99349f02bee49da71d40c68172
syamiimie <2025168815@student.uitm.edu.my>
2026-01-01 18:32:47 +0800

    update stats in StaffHomepage.html

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const reportRoutes = require("./routes/report.routes");
 const attendanceRoutes = require("./routes/attendance.routes");
 const paymentRoutes = require("./routes/payment.routes");
 const attendanceReportRoutes = require("./routes/attendanceReport.routes");
-
+const dashboardRoutes = require("./routes/dashboard.routes");
 
 const app = express();
 const PORT = 3000;
@@ -28,7 +28,7 @@ app.use("/enrollments", require("./routes/enrollment.routes"));
 app.use("/attendance", attendanceRoutes);
 app.use("/payment", paymentRoutes);
 app.use("/reports/attendance", attendanceReportRoutes);
-
+app.use("/dashboard", dashboardRoutes);
 
 app.listen(PORT, () => {
   console.log(`🚀 API running at http://localhost:${PORT}`);
