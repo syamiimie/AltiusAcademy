3b9ae3a6e2e351bd96bd5acc4e536e25cd5e6be3
syamiimie <2025168815@student.uitm.edu.my>
2026-01-02 04:41:18 +0800

    edit gui attendance

--- backend/routes/attendance.routes.js
+++ backend/routes/attendance.routes.js
@@ -2,6 +2,10 @@ const express = require("express");
 const router = express.Router();
 const c = require("../controllers/attendance.controller");
 
+// 🔥 OPTION 2 – SPECIFIC ROUTE FIRST
+router.get("/class/:class_id/students", c.getStudentsByClass);
+
+// BASIC CRUD
 router.get("/", c.getAllAttendance);
 router.get("/:id", c.getAttendanceById);
 router.post("/", c.addAttendance);
